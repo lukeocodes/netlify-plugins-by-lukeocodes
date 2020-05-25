9a0fd633cbc69c021d11f84e6fc0d64d7f3c8a86
Luke Oliff <luke@lukeoliff.com>
2020-05-25 11:05:46 +0100

    inline nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,3 +5,27 @@
     <li><nuxt-link to="/private">Not Indexed</nuxt-link></li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  margin-top: 16px;
+  list-style: none;
+}
+
+li {
+  display: inline;
+}
+
+li:after {
+  content: ' ›› ';
+  display: inline;
+  font-size: 1.2em;
+  color: #aaa;
+
+  padding: 0 0.0725em 0 0.15em;
+}
+
+li:last-child:after {
+  content: '';
+}
+</style>
